a354e8a96c2a2017bee6883f809bfe45a32c2cf6
Sergio1981 <scdesenvolvedor@gmail.com>
2016-05-27 21:59:21 -0300

    Criação de edição de banco de dados local com web sql storage

--- www/js/controller/app_controller.js
+++ www/js/controller/app_controller.js
@@ -1,12 +1,35 @@
 (function() {
 	"use strict";
 
-	angular.module("myApp").controller("initCtrl", function($scope, Data, $ionicModal, $location){
+	angular.module("myApp").controller("initCtrl", function($scope, Data, $ionicModal, $location, DBLocal){
 		$scope.home = "Contatos";
 		$scope.perfil = "Perfil";
 
 		$scope.contatos = [];
 
+		
+		DBLocal.localdb();
+
+		var nome = "Sergio";
+		var email = "joao@email.com";
+		var senha = "654321";
+
+		// INSERINDO DADOS LOCALMENTE
+		// DBLocal.db.transaction(function(res) {
+		// 	res.executeSql("INSERT INTO USER(nome, email, senha) VALUES(?,?,?);", [nome, email, senha]);
+		// });
+
+		//RECUPERANDO DADO LOCAL
+		DBLocal.db.transaction(function(res) {
+			var q = "SELECT * FROM USER";
+			res.executeSql(q, null, function(i, data) {
+				$scope.nome = "Olá " + data.rows.item(1).nome;
+			});
+		});
+
+		DBLocal.db.transaction(function(res) {
+			res.executeSql("DELETE FROM USER WHERE nome = ?;",[nome]);
+		});
 
 		var getData = function(){
 			var  params = {
@@ -14,7 +37,7 @@
 				token:"1f3d2gs3f2fg3as2fdg3re2t1we46er45"
 			};
 
-			Data.getData(params).success(function(data){
+		Data.getData(params).success(function(data){
 				$scope.contatos = data;
 
 			}).error(function(data){
